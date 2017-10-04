5301ac8298279cf54b39278204e07b8f64f05494
hauduagit <schoolland1234@gmail.com>
2017-10-05 00:50:13 +0700

    modify view ThemSanPham & model DanhMuc

--- ShowMuaBan/Models/DanhMuc.cs
+++ ShowMuaBan/Models/DanhMuc.cs
@@ -12,16 +12,17 @@ namespace ShowMuaBan.Models
         [Key]
         public int NguoiDangId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Phải điền nhãn hiệu")]
         [StringLength(10)]
         [Display(Name ="Nhãn hiệu")]
         public string NhanHieu { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Phải điền tên sản phẩm")]
         [StringLength(50)]
         [Display(Name ="Tên sản phẩm")]
         public string TenSanPham { get; set; }
 
+        [Required(ErrorMessage = "Phải điền ngày đăng vào")]
         [Display(Name = "Ngày đăng")]
         public DateTime NgayDangSP { get; set; }
 
